cb84b9341fe70f456168694a5095899337c22baf
alexandreLamarre <alex7285@gmail.com>
2022-04-16 16:08:02 -0400

    Object oritened API (operators) for mock paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 /// Drive exists but is not accessible
 static _WINERROR_NOT_READY: i32 = 21;
 /// (Original python implementation description) : fix for bpo-35306
@@ -28,6 +30,41 @@ pub trait PurePath {
 pub trait Path {
     fn new(path: &str) -> Self;
     fn to_str(&self) -> &str;
+}
+
+/// Mock filesystem path for testing Path trait
+struct MockPath {
+    path: String,
+}
+
+impl Path for MockPath {
+    fn new(path: &str) -> Self {
+        MockPath {
+            path: path.to_string(),
+        }
+    }
+
+    fn to_str(&self) -> &str {
+        self.path.as_str()
+    }
+}
+
+impl ToString for MockPath {
+    fn to_string(&self) -> String {
+        self.path.clone()
+    }
+}
+
+/// Implement '/' operator for Path abstractions
+/// FIXME: this should avoid using Box to wrap the arguments because it
+/// produces a more verbose user API
+impl ops::Div<Box<dyn ToString>> for MockPath {
+    type Output = MockPath;
+    fn div(self, rhs: Box<dyn ToString>) -> MockPath {
+        MockPath {
+            path: format!("{}/{}", self.path, rhs.to_string()),
+        }
+    }
 }
 
 /// Pure path implementation for non-windows systems.
@@ -66,7 +103,38 @@ mod tests {
         wild_question : ("?", true),
         wild_bracket : ("[", true),
         wild_nested : ("dir_name/filename",false),
-        wild_nested_n_star : ("dir_name/*filename",true),
+        wild_nested_n_star : ("dir_name/*.extension",true),
         // wild_escaped : ('\*',false), FIXME: this is not supported by rust as an escape character
+    }
+
+    macro_rules! mock_path_tests {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (input, expected) = $value;
+                    let path = MockPath::new(input);
+                    assert_eq!(path.to_str(), expected);
+                }
+            )*
+        }
+    }
+
+    mock_path_tests! {
+        path_empty : ("", ""),
+        path_slash : ("/", "/"),
+        // path_slash_slash : ("//", "/"), #FIXME : should be supported, but isn't
+        path_name : ("dirname/filename", "dirname/filename"),
+    }
+
+    #[test]
+    fn mock_path_concat() {
+        let path = MockPath::new("dirname/subdir");
+        let concat = path / Box::new(MockPath::new("filename.extension"));
+        assert_eq!(concat.to_str(), "dirname/subdir/filename.extension");
+
+        let path2 = MockPath::new("dirname/subdir");
+        let concat2 = path2 / Box::new("filename.extension");
+        assert_eq!(concat2.to_str(), "dirname/subdir/filename.extension");
     }
 }
